ee34b21bcb7c7481e67bb5c1d18b88e7c68076b6
lyyitau <43318969+lyyitau@users.noreply.github.com>
2021-11-17 15:18:35 +0100

    clouds scale separately

--- code/clouds/src/App.vue
+++ code/clouds/src/App.vue
@@ -1,45 +1,73 @@
 
- <template>
-  <div id="app">
-    <div class="content_img">
-      <div class="type"></div>
-      <!-- <h3>Transportation</h3> -->
-        <img src="./assets/cloud_transportation.png" width='100%' align="bottom" alt="CO2 emission through transportation is caused at early stages of your product's life cycle. In particular, raw materials have to be excavated and transported to the location of production. In some cases, production may be split into different locations, leading to even more transportation needed. All of this causes CO2 to be set free. Further, CO2 is emitted during the transportation of the product to your home.">
-      <div class="textbox">
-        CO2 emission through transportation is caused at early stages of your product's life cycle. In particular, raw materials have to be excavated and transported to the location of production. In some cases, production may be split into different locations, leading to even more transportation needed. All of this causes CO2 to be set free. Further, CO2 is emitted during the transportation of the product to your home.
-      </div>
-    </div>
-    <div class="content_img">
-      <div class="type"></div>
-      <img src="./assets/cloud_materials.png" width='100%' align="bottom" alt="The production of raw material is linked to CO2 emissions through the process' energy consumption.">
-      <div class="textbox">
-        The production of raw material is linked to CO2 emissions through the process' energy consumption.
-      </div>
-    </div>
-    <div class="content_img">
-      <div class="type"></div>
-      <img src="./assets/cloud_production.png" width='100%' align="bottom" alt="The processing of raw materials to the final good can be linked with a lot of energy consumption. In addition, other resources, such as water, may be needed during the production process. Thus, CO2 consumption may also be linked to the production of goods.">
-      <div class="textbox">
-        The processing of raw materials to the final good can be linked with a lot of energy consumption. In addition, other resources, such as water, may be needed during the production process. Thus, CO2 consumption may also be linked to the production of goods.
-      </div>
-    </div>
-    <div class="content_img">
-      <div class="type"></div>
-      <img src="./assets/cloud_endoflife.png" width='100%' align="bottom" alt="Have you ever thought oc the recyclability of the products you're buying? If a product has a longer life time and/or can be fully recycled, it is considered to emit less CO2.">
-      <div class="textbox">
-        Have you ever thought oc the recyclability of the products you're buying? If a product has a longer life time and/or can be fully recycled, it is considered to emit less CO2.
-      </div>
-    </div>
-  </div>
+<template>
+ <div id="app">
+   <div class="content_img">
+     <div class="type"></div>
+     <!-- <h3>Transportation</h3> -->
+       <img class="transportation" src="./assets/cloud_transportation.png" :style='cssVars' alt="CO2 emission through transportation is caused at early stages of your product's life cycle. In particular, raw materials have to be excavated and transported to the location of production. In some cases, production may be split into different locations, leading to even more transportation needed. All of this causes CO2 to be set free. Further, CO2 is emitted during the transportation of the product to your home.">
+     <div class="textbox">
+       CO2 emission through transportation is caused at early stages of your product's life cycle. In particular, raw materials have to be excavated and transported to the location of production. In some cases, production may be split into different locations, leading to even more transportation needed. All of this causes CO2 to be set free. Further, CO2 is emitted during the transportation of the product to your home.
+     </div>
+   </div>
+   <div class="content_img">
+     <div class="type"></div>
+     <img class="materials" src="./assets/cloud_materials.png" :style='cssVars' alt="The production of raw material is linked to CO2 emissions through the process' energy consumption.">
+     <div class="textbox">
+       The production of raw material is linked to CO2 emissions through the process' energy consumption.
+     </div>
+   </div>
+   <div class="content_img">
+     <div class="type"></div>
+     <img class="production" src="./assets/cloud_production.png" :style='cssVars' align="bottom" alt="The processing of raw materials to the final good can be linked with a lot of energy consumption. In addition, other resources, such as water, may be needed during the production process. Thus, CO2 consumption may also be linked to the production of goods.">
+     <div class="textbox">
+       The processing of raw materials to the final good can be linked with a lot of energy consumption. In addition, other resources, such as water, may be needed during the production process. Thus, CO2 consumption may also be linked to the production of goods.
+     </div>
+   </div>
+   <div class="content_img">
+     <div class="type"></div>
+     <img class= "endoflife" src="./assets/cloud_endoflife.png" :style='cssVars' alt="Have you ever thought oc the recyclability of the products you're buying? If a product has a longer life time and/or can be fully recycled, it is considered to emit less CO2.">
+     <div class="textbox">
+       Have you ever thought oc the recyclability of the products you're buying? If a product has a longer life time and/or can be fully recycled, it is considered to emit less CO2.
+     </div>
+   </div>
+ </div>
 
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
+ name: 'app',
+ props: {
 
+  heighttrans: {
+  type: Number,
+  default: 100
+},
+  heightmat: {
+  type: Number,
+  default: 50
+},
+  heightprod: {
+  type: Number,
+  default: 80
+},
+  heighteol: {
+  type: Number,
+  default: 120
+              }
+ },
+ data () {
+   return {
+   };
+ },
+ computed: {
+    cssVars() {
+      return {
+        '--heighttrans': this.heighttrans +'%' ,
+        '--heightmat': this.heightmat + '%',
+        '--heightprod': this.heightprod + '%',
+        '--heighteol': this.heighteol + '%'
+      }
     }
   }
 }
@@ -82,6 +110,22 @@ a {
 }
 
 
+.transportation{
+  height: var(--heighttrans);
+  align: bottom;
+}
+.materials{
+  height: var(--heightmat);
+  align: bottom;
+}
+.production{
+  height: var(--heightprod);
+  align: bottom;
+}
+.endoflife{
+  height: var(--heighteol);
+  align: bottom;
+}
 
 /*copied from from https://makitweb.com/html-how-to-show-text-above-image-on-hover/ and adjusted for our purposes*/
 /* Parent Container */
